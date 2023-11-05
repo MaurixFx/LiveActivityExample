3262fa35c93b866613cb3be053b2a3234a5c0bcc
Mauricio Figueroa <maurixfx@gmail.com>
2023-11-05 15:28:54 +0100

    Initial Commit

---
+++ LiveActivityExample/LiveActivityExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveActivityExampleApp.swift
+//  LiveActivityExample
+//
+//  Created by Mauricio Figueroa on 05-11-23.
+//
+
+import SwiftUI
+
+@main
+struct LiveActivityExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
